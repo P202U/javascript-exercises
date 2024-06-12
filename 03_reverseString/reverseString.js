320d569268f6f54be04a95ef09bd929361570c01
Piyush <156369604+P202U@users.noreply.github.com>
2024-06-13 00:55:28 +0530

    Use split and join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,6 @@
 const reverseString = function (string) {
-    let reversedString = '';
-    if (string === '') return '';
-    for (let i = string.length - 1; i >= 0; i--) {
-        reversedString += string[i];
-    }
-    return reversedString;
+    return string.split('').reverse().join('');
 };
-// npm test reverseString.spec.js
+
 // Do not edit below this line
 module.exports = reverseString;
