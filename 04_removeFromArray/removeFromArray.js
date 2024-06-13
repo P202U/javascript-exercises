cd4abf24e453db70a88e4f475b6846b087a29317
Piyush <156369604+P202U@users.noreply.github.com>
2024-06-14 02:15:57 +0530

    Rest and filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...removable) {
+    for (let remove of removable) {
+        arr = arr.filter(item => item !== remove)
+    }
+    return arr;
 };
-
+// npm test removeFromArray.spec.js
 // Do not edit below this line
 module.exports = removeFromArray;
