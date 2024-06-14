7887af7575147922d7070fe3c3c6fc64e9a5ddb5
Piyush <156369604+P202U@users.noreply.github.com>
2024-06-15 01:17:24 +0530

    Convert into array for efficiency

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const punctuation = ['.', ',', ';', ':', '!', '?', '"', "'", '(', ')', '[', ']', '{', '}', '-', '_', ' ', " "];
+    const stringArray = string.toLowerCase().split(''); // Convert to Array
 
+    // Filter out punctuation
+    const filteredArray = stringArray.filter(item => !punctuation.includes(item));
+
+    // Check for palindrome
+    let i = 0;
+    let j = filteredArray.length - 1;
+    while (i < j) {
+        if (filteredArray[i] !== filteredArray[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
