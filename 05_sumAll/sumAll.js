447806dfee40cf2eb42faf9b693f4872e76fa9e1
Piyush <156369604+P202U@users.noreply.github.com>
2024-06-14 10:38:25 +0530

    Consecutive sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    return (start >= 0 && end >= 0 && typeof start === 'number' && typeof end === 'number') ?
+        ((start + end - 1) / 2 * (start + end)) :
+        'ERROR';
 };
 
 // Do not edit below this line
