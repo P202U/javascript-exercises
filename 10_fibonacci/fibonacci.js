7ecb25d38a9b2a31170606af55dfaad1c849a39a
Piyush <156369604+P202U@users.noreply.github.com>
2024-06-17 16:30:58 +0530

    Calculate nth fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let currentValue = 0;
+    let nextValue = 1;
 
+    if (n < 0) return `OOPS`;
+    else if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else if (n == 2) return 1;
+    else {
+        while (n > 1) {
+            let sum = currentValue + nextValue;
+            currentValue = nextValue;
+            nextValue = sum;
+            n--;
+        }
+    }
+    return nextValue;
 };
 
 // Do not edit below this line
