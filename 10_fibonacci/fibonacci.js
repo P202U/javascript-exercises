ddfb5045cb7482b2410716ba872a16cf863abaf9
Piyush <156369604+P202U@users.noreply.github.com>
2024-06-17 16:38:20 +0530

    Nth fibonacci using recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,20 +1,10 @@
 const fibonacci = function (n) {
-    let currentValue = 0;
-    let nextValue = 1;
-
-    if (n < 0) return `OOPS`;
+    if (n < 0) return `OOPS`
     else if (n == 0) return 0;
     else if (n == 1) return 1;
-    else if (n == 2) return 1;
     else {
-        while (n > 1) {
-            let sum = currentValue + nextValue;
-            currentValue = nextValue;
-            nextValue = sum;
-            n--;
-        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
-    return nextValue;
 };
 
 // Do not edit below this line
