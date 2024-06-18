e5e37c2922dc3704d201778d136739dd6e360bfc
Piyush <156369604+P202U@users.noreply.github.com>
2024-06-19 02:06:14 +0530

    Calculate maximum age for people without yearOfDeath

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((oldestPerson, person) => {
+        const age = (!person.yearOfDeath) ? new Date().getFullYear() - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth;
+        if (!oldestPerson || age > oldestPerson.age) {
+            return { person, age };
+        } else {
+            return oldestPerson;
+        }
+    }, null).person;
 };
 
 // Do not edit below this line
